2b5b2302d35ab5a39a911981c6fafa1ccbff4f60
Matt Fava <matt.b.fava@gmail.com>
2020-08-13 15:30:49 -0400

    build

---
+++ build/precache-manifest.400757dd0a6756baef7b2fc534fe0d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bbb699e6befd562c6a56a947098c1d",
+    "url": "/HW_19/index.html"
+  },
+  {
+    "revision": "fad84ec66090e3d52308",
+    "url": "/HW_19/static/css/main.3bad0b69.chunk.css"
+  },
+  {
+    "revision": "efba788d4db2765af108",
+    "url": "/HW_19/static/js/2.4c1f2a66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW_19/static/js/2.4c1f2a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad84ec66090e3d52308",
+    "url": "/HW_19/static/js/main.b421b91b.chunk.js"
+  },
+  {
+    "revision": "23dfd5792379efd71992",
+    "url": "/HW_19/static/js/runtime-main.b8939e85.js"
+  }
+]);
